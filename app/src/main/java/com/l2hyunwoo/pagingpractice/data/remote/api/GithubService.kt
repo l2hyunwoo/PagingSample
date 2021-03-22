33f272dfcbee028a73ef072df7d1c15183daa2ae
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-22 23:35:43 +0900

    [FEAT] : DataSource Layer Modeling

---
+++ app/src/main/java/com/l2hyunwoo/pagingpractice/data/remote/api/GithubService.kt
@@ -0,0 +1,13 @@
+package com.l2hyunwoo.pagingpractice.data.remote.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GithubService {
+    @GET("search/repositories?sort=stars")
+    suspend fun searchRepos(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") itemsPerPage: Int
+    )
+}
